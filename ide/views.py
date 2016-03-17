2f31c8a80090fbbbdac64b3fc71fc1ac5eb60ce7
Timur Iskhakov <iskhakovt@gmail.com>
2016-03-17 20:08:36 +0300

    Login templated added.

--- ide/views.py
+++ ide/views.py
@@ -1,10 +1,12 @@
 # Copyright (c) Timur Iskhakov.
 
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .docker import Docker
 
 
+@login_required
 def index(request, document):
     docker = Docker()
     docker.run.delay()
